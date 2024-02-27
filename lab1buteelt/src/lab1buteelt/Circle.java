5b96d720a5028cc8895354530f46bc07910fd157
Sodkhuu1 <151498355+Sodkhuu1@users.noreply.github.com>
2024-02-27 18:45:56 +0800

    web 6:45

--- lab1buteelt/src/lab1buteelt/Circle.java
+++ lab1buteelt/src/lab1buteelt/Circle.java
@@ -55,7 +55,7 @@ public class TaskManager {
 
         while (true) {
             System.out.println("\nTask Manager Menu:");
-            System.out.println("1. Add Task");
+            System.out.println("1. ажил нэмэх");
             System.out.println("2. Remove Task");
             System.out.println("3. Display Tasks");
             System.out.println("4. Mark Task as Completed");
